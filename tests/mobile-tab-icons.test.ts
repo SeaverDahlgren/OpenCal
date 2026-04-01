61b43c41d22a0c6eedf61b3717a8311af5deaca7
Seaver Dahlgren <12dahlgren12@gmail.com>
2026-04-01 09:05:59 -0700

    chore(mobile): remove legacy expo app config

--- tests/mobile-tab-icons.test.ts
+++ tests/mobile-tab-icons.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { getSettingsTabIcon } from "../apps/mobile/src/navigation/tab-icons.ts";
+import { getSettingsTabIcon } from "../apps/mobile/src/navigation/tab-icons.js";
 
 describe("mobile tab icons", () => {
   it("uses a vector gear icon for the settings tab", () => {
